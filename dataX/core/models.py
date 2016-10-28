4836c13802a2be501932b0d13ca8d88e2237914c
paulosantosjunior <paulossjunior@gmail.com>
2016-10-28 09:12:07 -0200

    atualizando com dados da naturalidade

--- dataX/core/models.py
+++ dataX/core/models.py
@@ -25,6 +25,7 @@ class Campus (models.Model):
     departamentos = models.ForeignKey(Departamento)
 
 
+
 class Aluno (models.Model):
     data_matricula = models.DateField()
     data_conclusao = models.DateField()
@@ -37,6 +38,19 @@ class Cor (models.Model):
 class Sexo (models.Model):
     sexo = models.CharField(max_length=50)
 
+class Municipio (models.Model):
+    nome = models.CharField(max_length=50)
+
+class Estado (models.Model):
+    nome = models.CharField(max_length=50)
+    municipios = models.ForeignKey(Municipio)
+
+class Endereco (models.Model):
+    logradouro = models.CharField(max_length=255)
+    municipio = models.ForeignKey(Municipio)
+    CEP = models.CharField(max_length=15)
+    data_cadastro = models.DateField()
+
 class Pessoa (models.Model):
     data_nascimento = models.DateField()
     data_conclusao_ensino_medio = models.DateField()
@@ -44,5 +58,6 @@ class Pessoa (models.Model):
     sexo = models.ForeignKey(Sexo)
     CEP = models.CharField(max_length=15)
     matriculas = models.ForeignKey(Aluno)
+    endereco = models.ForeignKey(Endereco)
 
 
