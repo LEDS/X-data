90a3a609c081cc25f75d14229ada80e0e1c94568
paulosantosjunior <paulossjunior@gmail.com>
2016-10-28 00:00:21 -0200

    modelo de dados finalizado

--- dataX/core/models.py
+++ dataX/core/models.py
@@ -4,21 +4,44 @@ from django.db import models
 
 # Create your models here.
 
+
+class Estrutura(models.Model):
+    nome = models.CharField(max_length=50)
+
+class Matriz_Curricular(models.Model):
+    nome = models.CharField(max_length=50)
+    estrutura = models.ForeignKey(Estrutura)
+
+class Curso (models.Model):
+    nome = models.CharField(max_length=50)
+    matrizes_curriculares = models.ForeignKey(Matriz_Curricular)
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=50)
+    cursos = models.ForeignKey(Curso)
+
+class Campus (models.Model):
+    nome = models.CharField(max_length=50)
+    departamentos = models.ForeignKey(Departamento)
+
+
 class Aluno (models.Model):
     data_matricula = models.DateField()
     data_conclusao = models.DateField()
+    matriz_curricular = models.ForeignKey(Matriz_Curricular)
+
 
 class Cor (models.Model):
     nome = models.CharField(max_length=50)
 
 class Sexo (models.Model):
-    sexo = models.CharField(max_length=50, default="Não Informado")
+    sexo = models.CharField(max_length=50)
 
 class Pessoa (models.Model):
     data_nascimento = models.DateField()
     data_conclusao_ensino_medio = models.DateField()
-    cor = models.OneToOneField(Cor)
-    sexo = models.OneToOneField(Sexo)
+    cor = models.ForeignKey(Cor)
+    sexo = models.ForeignKey(Sexo)
     CEP = models.CharField(max_length=15)
     matriculas = models.ForeignKey(Aluno)
 
