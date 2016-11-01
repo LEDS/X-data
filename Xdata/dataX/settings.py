72857a533741299d9934b57068fb74216e407352
paulosantosjunior <paulossjunior@gmail.com>
2016-11-01 13:06:30 -0200

    Modificando o nome da pasta da aplicacao e configurando para trabalhar com postgresql

--- Xdata/dataX/settings.py
+++ Xdata/dataX/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'dataX.urls'
+ROOT_URLCONF = 'Xdata.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'dataX.wsgi.application'
+WSGI_APPLICATION = 'Xdata.wsgi.application'
 
 
 # Database
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'dataX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'xdata',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD': '123',
+        'PORT': '5432',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
--- Xdata/dataX/wsgi.py
+++ Xdata/dataX/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dataX.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Xdata.settings")
 
 application = get_wsgi_application()
