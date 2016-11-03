dc10b42f50b2c974df6319904097d1264a78163e
paulosantosjunior <paulossjunior@gmail.com>
2016-11-03 13:28:09 -0200

    Modiciando os modelos, transformacoes, adicionando admin

--- Xdata/core/admin.py
+++ Xdata/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cor, Sexo
+
+class CorAdmin(admin.ModelAdmin):
+    list_display = ('nome','sigla')
+    list_filter = ['nome','sigla']
+    search_fields = ['nome']
+
+
+class SexoAdmin(admin.ModelAdmin):
+    list_display = ('nome','sigla')
+    list_filter = ['nome','sigla']
+    search_fields = ['nome']
+
+admin.site.register(Sexo,SexoAdmin)
+
+admin.site.register(Cor,CorAdmin)
+
--- Xdata/core/models.py
+++ Xdata/core/models.py
@@ -1,69 +1,61 @@
-from __future__ import unicode_literals
-
 from django.db import models
-
-
-class Estrutura(models.Model):
-    nome = models.CharField(max_length=50)
-
-class Matriz_Curricular(models.Model):
-    nome = models.CharField(max_length=50)
-    estrutura = models.ForeignKey(Estrutura)
-
-class Curso (models.Model):
-    nome = models.CharField(max_length=50)
-    matrizes_curriculares = models.ForeignKey(Matriz_Curricular)
-
-class Departamento(models.Model):
-    nome = models.CharField(max_length=50)
-    cursos = models.ForeignKey(Curso)
+from django.utils import timezone
 
 class Campus (models.Model):
     nome = models.CharField(max_length=50)
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=255)
+    campus = models.ForeignKey(Campus)
+
+class Curso (models.Model):
+    nome = models.CharField(max_length=255)
     departamentos = models.ForeignKey(Departamento)
+
+class Cor (models.Model):
+    nome = models.CharField(max_length=50)
+    sigla = models.CharField(max_length=3)
+
+class Sexo (models.Model):
+    nome = models.CharField(max_length=50)
+    sigla = models.CharField(max_length=1)
+
+class Periodo(models.Model):
+    ano = models.IntegerField()
+    semestre = models.IntegerField()
 
 class Situacao (models.Model):
     nome = models.CharField(max_length=50)
 
-class Periodo (models.Model):
-     ano = models.IntegerField()
-     semestre = models.IntegerField()
-
-class Forma_Ingresso(models.Model):
-    nome = models.CharField(max_length=100)
+class Pessoa(models.Model):
+    data_nascimento = models.DateField()
+    data_conclusao_ensino_medio = models.DateField()
+    codigo_social = models.CharField(max_length=255)
+    cor = models.ForeignKey(Cor)
+    sexo = models.ForeignKey(Sexo)
 
 class Aluno (models.Model):
-    periodo_matricula = models.OneToOneField(Periodo,related_name="periodo_matricula")
-    periodo_conclusao = models.OneToOneField(Periodo,related_name="periodo_conclusao", blank=True,null=True,)
-    data_conclusao = models.DateField()
-    matriz_curricular = models.ForeignKey(Matriz_Curricular)
+    data_conclusao = models.DateField(blank=True,null=True,)
+    curso = models.ForeignKey(Curso)
+    pessoa = models.ForeignKey(Pessoa)
+
+class Situacao_Aluno(models.Model):
+    data = models.DateField()
+    aluno = models.ForeignKey(Aluno)
     situacao = models.ForeignKey(Situacao)
-    forma_ingresso = models.ForeignKey(Forma_Ingresso)
-
-class Cor (models.Model):
-    nome = models.CharField(max_length=50)
-
-class Sexo (models.Model):
-    sexo = models.CharField(max_length=50)
-
-class Municipio (models.Model):
-    nome = models.CharField(max_length=50)
 
 class Estado (models.Model):
     nome = models.CharField(max_length=50)
-    municipios = models.ForeignKey(Municipio)
+
+
+class Municipio (models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado)
 
 class Endereco (models.Model):
     logradouro = models.CharField(max_length=255)
     municipio = models.ForeignKey(Municipio)
     CEP = models.CharField(max_length=15)
-    data_cadastro = models.DateField()
+    data_cadastro = models.DateField(default=timezone.now)
+    pessoa = models.ForeignKey(Pessoa)
 
-class Pessoa (models.Model):
-    data_nascimento = models.DateField()
-    data_conclusao_ensino_medio = models.DateField()
-    codigo_social = models.CharField(max_length=255)
-    cor = models.OneToOneField(Cor)
-    sexo = models.OneToOneField(Sexo)
-    matriculas = models.ForeignKey(Aluno)
-    endereco = models.ForeignKey(Endereco)
