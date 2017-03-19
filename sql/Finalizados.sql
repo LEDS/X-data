be48cecc2bda72990d09c45d544f92825942d35c
MartinsJunior <junior.m10@hotmail.com>
2017-03-19 17:58:53 -0300

    Add files via upload

---
+++ sql/Finalizados.sql
@@ -0,0 +1,14 @@
+create view FINALIZADOS as
+select count(*) as quantidade, core_curso.id as curso_id, core_periodo.id as periodo_id, core_curso.nome, core_periodo.ano, core_periodo.semestre
+from core_aluno 
+inner join core_situacao_aluno
+on core_situacao_aluno.aluno_id=core_aluno.id
+inner join core_situacao
+on core_situacao_aluno.situacao_id=core_situacao.id
+inner join core_periodo
+on core_situacao_aluno.periodo_id=core_periodo.id
+inner join core_curso
+on core_aluno.curso_id=core_curso.id
+where core_situacao.nome in ('Conclu�do','Evas�o','Transferido Externo','Cancelado')
+group by core_curso.nome, core_periodo.ano, core_periodo.semestre,core_curso.id, core_periodo.id
+order by core_periodo.ano, core_periodo.semestre
